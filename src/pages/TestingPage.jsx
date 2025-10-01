543f5b7dfb0648eb97a9340464ddc84380cb54ea
Your Name <your-email@example.com>
2025-10-01 13:52:48 +0530

    chnaged

--- src/pages/TestingPage.jsx
+++ src/pages/TestingPage.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import Testing from '../component/it/Testing'
-import Navbar from '../component/navbar/Navbar'
-import Footer from '../component/footer/Footer'
+// import React from 'react'
+// import Testing from '../component/it/Testing'
+// import Navbar from '../component/navbar/Navbar'
+// import Footer from '../component/footer/Footer'
 
-const TestingPage = () => {
-  return (
-<>
-<Navbar/>
-<Testing/>
-<Footer/>
-</>  )
-}
+// const TestingPage = () => {
+//   return (
+// <>
+// <Navbar/>
+// <Testing/>
+// <Footer/>
+// </>  )
+// }
 
-export default TestingPage
+// export default TestingPage
