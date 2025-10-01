fe37b1ec0f02f9e59f005734667c290f6611cc65
Your Name <your-email@example.com>
2025-10-01 11:54:28 +0530

    Testing add Footer section

--- src/pages/TestingPage.jsx
+++ src/pages/TestingPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Testing from '../component/it/Testing'
 import Navbar from '../component/navbar/Navbar'
+import Footer from '../component/footer/Footer'
 
 const TestingPage = () => {
   return (
 <>
 <Navbar/>
 <Testing/>
+<Footer/>
 </>  )
 }
 
